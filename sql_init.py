445673eafc894a5889663f71246fc80bc1e88566
Oleksiy Budilovsky <alexbudy@gmail.com>
2023-08-20 14:10:24 -0700

    add db initialization

---
+++ sql_init.py
@@ -0,0 +1,18 @@
+from multiprocessing import connection
+import sqlite3
+
+migration_file_path = "migration.sql"
+
+def initialize_db(database_path: str):
+  conn = sqlite3.connect(database_path)
+  
+  cursor = conn.cursor()
+  with open(migration_file_path) as f:
+    script = f.read()
+    cursor.executescript(script)
+  
+
+if __name__ == "__main__":
+  db_path = "my_db.db"
+  
+  initialize_db(db_path)
