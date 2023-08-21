3dd56248f503fefc7cc93bc236eb0b6663f4d476
Oleksiy Budilovsky <alexbudy@gmail.com>
2023-08-21 12:14:51 -0700

    create profile and ranking table, add arg parsing, fk constrains not working yet

--- migration.sql
+++ migration.sql
@@ -2,6 +2,21 @@ CREATE TABLE IF NOT EXISTS nouns (
   id INTEGER PRIMARY KEY,
   gender TEXT NOT NULL,
   spanish TEXT NOT NULL,
-  english TEXT NOT NULL,
-  score INTEGER NOT NULL DEFAULT 0
+  english TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS profiles (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ranking (
+  profile_id INTEGER NOT NULL,
+  noun_id INTEGER NOT NULL,
+  en_to_es REAL DEFAULT 0,
+  es_to_en REAL DEFAULT 0,
+  FOREIGN KEY(profile_id) REFERENCES profiles(id),
+  FOREIGN KEY(noun_id) REFERENCES nouns(id)
 )
--- sql_init.py
+++ sql_init.py
@@ -1,10 +1,13 @@
 from multiprocessing import connection
 import sqlite3
+import argparse
+import os
 
 migration_file_path = "migration.sql"
 
 def initialize_db(database_path: str):
   conn = sqlite3.connect(database_path)
+  conn.execute("PRAGMA foreign_keys = ON;")
   
   cursor = conn.cursor()
   with open(migration_file_path) as f:
@@ -14,5 +17,13 @@ def initialize_db(database_path: str):
 
 if __name__ == "__main__":
   db_path = "my_db.db"
+  
+  parser = argparse.ArgumentParser()
+  
+  parser.add_argument("--drop-db", default=False, action='store_true')
+  
+  args = parser.parse_args()
+  if args.drop_db:
+    os.remove(db_path)
   
   initialize_db(db_path)
